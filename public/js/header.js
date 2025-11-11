0478fa6098a8296fb803329ed0b05092e2375c6a
Martin Tuskevicius <martin.tuskevicius@gmail.com>
2025-11-11 12:01:31 -0800

    Rewrote the header code as JavaScript.

---
+++ public/js/header.js
@@ -0,0 +1,27 @@
+const headerElement = document.createElement("header");
+const navElement = document.createElement("nav");
+const linkListElement = document.createElement("ul");
+const headerImage = document.createElement("img");
+headerImage.src = "./images/header_full.jpg";
+headerImage.alt = "Header";
+
+function addLink(text, href, id) {
+  const linkItem = document.createElement("li");
+  const link = document.createElement("a");
+  link.textContent = text;
+  link.href = href;
+  if (id) {
+    link.id = id;
+  }
+  linkItem.appendChild(link);
+  linkListElement.appendChild(linkItem);
+}
+
+addLink("Home", "./index.html");
+addLink("Etsy", "https://www.etsy.com/shop/ChooseLinen", "etsy-link");
+addLink("Contact", "./contact.html");
+
+navElement.appendChild(linkListElement);
+headerElement.appendChild(navElement);
+headerElement.appendChild(headerImage);
+document.body.appendChild(headerElement);
